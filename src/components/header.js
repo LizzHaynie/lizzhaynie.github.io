1144ea29067ab916d68c930d6decaf1462c817e8
lizzhaynie <ehaynie@ggc.edu>
2022-07-06 14:11:32 -0700

    Animation shouldn't appear on mount

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,16 @@ import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
     const [mobileOpened, setMobileOpened] = useState(false);
+    const [mounted, setMounted] = useState(false);
     const location = useLocation();
 
     useEffect(() => {
         document.body.scrollTop = document.documentElement.scrollTop = 0
     }, [location.pathname]);
+
+    useEffect(() => {
+        !!mobileOpened && setMounted(true);
+    }, [mobileOpened]);
 
     return (
         <>
@@ -16,7 +21,7 @@ export default function Header() {
                     <div className="header-left-links">
                         <div onClick={() => setMobileOpened(!mobileOpened)} className={`menu-button-container ${mobileOpened ? "menu-opened-view" : ""}`}>
                             <div className="menu-button" />
-                            <ul className={`menu ${mobileOpened ? "menu-visible" : "menu-hidden"}`}>
+                            <ul className={`menu ${mobileOpened ? "menu-visible" : !!mounted && !mobileOpened ? "menu-close-anim" : "menu-hidden"}`}>
                                 <li className={!location.pathname.includes('projects') ? "active-menu" : ""}><Link to="/">Home</Link></li>
                                 <li className={location.pathname.includes('projects') ? "active-menu" : ""}><Link to="/projects">Projects</Link></li>
                             </ul>
